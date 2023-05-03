83e836b12338d199e5cf5da97d5eecc1426a8c8e
Carlos Leon <midoriya3612@gmail.com>
2023-05-03 12:42:10 -0500

    update

---
+++ src/main/java/com/acme/webserviceslinerepair/technician/resource/TechnicianResource.java
@@ -0,0 +1,20 @@
+package com.acme.webserviceslinerepair.technician.resource;
+
+import lombok.*;
+
+@Setter
+@Getter
+@With
+@NoArgsConstructor
+@AllArgsConstructor
+public class TechnicianResource {
+    private Long id;
+    private String username;
+    private String names;
+    private String lastNames;
+    private String address;
+    private String email;
+    private String password;
+    private String cellPhoneNumber;
+    private String urlToProfile;
+}
