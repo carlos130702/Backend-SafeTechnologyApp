8d002374f6dbe35f7bf4307738d81dbd1cc332e1
Carlos Leon <midoriya3612@gmail.com>
2023-05-15 13:10:59 -0500

    update

--- src/main/java/com/acme/webserviceslinerepair/shared/configuration/OpenApiConfiguration.java
+++ src/main/java/com/acme/webserviceslinerepair/shared/configuration/OpenApiConfiguration.java
@@ -43,14 +43,4 @@ public class OpenApiConfiguration implements WebMvcConfigurer {
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(3600);
-    }
 }
